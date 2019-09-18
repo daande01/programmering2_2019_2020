1dc08818747a8c0c0051b6660461aff6969b79c2
daande01 <daniel.evert.andersson@gmail.com>
2019-09-18 10:53:23 +0200

    new file

---
+++ Programmering2_2019_2020/src/tiddemo_tidpunkt/Räknare.java
@@ -0,0 +1,60 @@
+package tiddemo_tidpunkt;
+
+public class Räknare {
+
+	private int räknare;
+	private int max;
+	private int min;
+
+	public Räknare(int r,int max, int min) {
+
+		if (r<=20 && r>=10) {
+
+			räknare= r;
+			this.max=max;
+			this.min=min;
+		}else {
+
+			System.out.println("du har angivit ett felaktigt värde");
+
+		}
+
+
+	}
+
+	public void öka() {
+
+		if (räknare<max) {
+		räknare++;
+		System.out.println(räknare);
+		}else {
+
+			System.out.println("du har nått max gränsen");
+		}
+	}
+	public void minska() {
+		if (räknare>min) {
+		räknare--;
+		System.out.println(räknare);
+		}else {
+
+			System.out.println("du har nått min gränsen");
+		}
+	}
+
+
+	public static void main(String[] args) {
+
+		Räknare r1= new Räknare(12,30,0);
+
+		for (int i = 0; i < 100; i++) {
+
+			r1.minska();
+
+		}
+
+
+	}
+
+
+}
