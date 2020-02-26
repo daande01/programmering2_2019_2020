0b8f173b3b47ed9d477a146fa36cadc9ee0300a2
daande01 <daniel.evert.andersson@gmail.com>
2020-02-26 12:06:38 +0100

    hus

--- Programmering2_2019_2020/src/hus/Hus.java
+++ Programmering2_2019_2020/src/hus/Hus.java
@@ -17,9 +17,41 @@ public  class Hus {
 
 	public static void main(String[] args) {
 
+		Hus h = new Hus();
+		Bostadshus b= new Bostadshus();
+		Flerfamiljshus f= new Flerfamiljshus();
+//		h.större(b);
+//		h=b;
+//		h=f;
+//
+//		//b=h;  // går ej fel håll
+//		b= (Bostadshus)h;  //farligt om h inte är ett bostadshus
+//
+//		if (h instanceof Bostadshus) {
+//			b= (Bostadshus)h;
+//		}
 
+		//__________komma åt dolda variabler i från main _______________
 
+//		Hus hh;
+//		Flerfamiljshus ff= new Flerfamiljshus();
+//
+//		ff.senasteRenovering=2018;
+//		hh=ff;
+//		hh.senasteRenovering=2020;
+		//____________polymorfism dynamiskbindning_______________
 
+		Bostadshus b1 = new Bostadshus();
+		Flerfamiljshus f1 = new Flerfamiljshus();
+		b1.bredd=10; b1.längd=20; b1.antalVåningar=3;
+		f1.bredd=10; f1.längd=20; f1.antalVåningar=3;
+
+		Bostadshus b2;
+		b2=f1;
+
+		System.out.println(b1.yta());
+		System.out.println(f1.yta());
+		System.out.println(b2.yta());
 
 	}
 
@@ -47,7 +79,7 @@ class Flerfamiljshus extends Bostadshus{
 	public void renovera (int yttre , int inre) {
 		// dolda instancvariabler använd super för att komma åt förälders dolda instansvariabel
 		super.senasteRenovering =yttre;
-		senasteRenovering = inre;
+		this.senasteRenovering = inre;
 	}
 	@Override  // kompilation kommer nu att kontrollera så att det finns en yta metod i superklassen
 	 public double yta() {
--- Programmering2_2019_2020/src/polymorfism2/AnimalInterface.java
+++ Programmering2_2019_2020/src/polymorfism2/AnimalInterface.java
@@ -1,48 +1,51 @@
 /**
- * 
+ *
  */
 package polymorfism2;
+
 
 interface AnimalInterface {
     String getName();
     String makeSound();
 }
- 
+
 abstract class Animal implements AnimalInterface {
     private final String name;
- 
+
     protected Animal(String name) {
         this.name = name;
     }
- 
-    public String getName() {
+
+    @Override
+	public String getName() {
         return this.name;
     }
- 
-    public String makeSound() {
+
+    @Override
+	public String makeSound() {
         return "";
     }
 }
- 
+
 class Cat extends Animal {
     public Cat(String name) {
         super(name);
     }
- 
+
     @Override
     public String makeSound() {
         return "Meooow";
     }
 }
- 
+
 class Dog extends Animal {
     public Dog(String name) {
         super(name);
     }
-    
+
     @Override
     public String makeSound() {
         return "Woof! Woof!";
     }
 }
- 
+
